03f4cd11ae25160996c65736fd92b7a10deed0ad
Bhupendra koli <114750201+BhupendraKoli@users.noreply.github.com>
2023-04-13 08:08:45 +0530

    Add files via upload

--- static.c
+++ static.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
-void increment();
+int increment();
 int main()
 {
-	
-	increment();
 	increment();
 	increment();
 	increment();
 	return 0;
-	
 }
-void increment()
+int increment()
 {
 	static int i=1;
 	printf("%d",i);
 	i=i+1;
-
 }
